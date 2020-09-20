a0f43b76a845c61e1fc7cce42a07c79f0a9e4ec9
tpyoknig <tpyoknig@gmaill.com>
2020-09-19 21:44:37 -0400

    Added Script, images and info to README
    Initial commit of the script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Tune-Linear-Pressure-Advance
-Tuneable Python Code to generate a Linear Advance/Pressure Advance gcode file for Marlin or Duet with reporting
+Tuneable Python Code to generate a Linear Advance/Pressure Advance gcode file for Marlin or Duet with reporting. This is a modification of a script that I found on the [Duet Forum](https://forum.duet3d.com/topic/6698/pressure-advance-calibration)
+
+# Getting Started
+
+## Editing the code
+- all tuneable items are at the very top between lines 5 and 38
+- Change the min/max values for Linear Advance
+- inspect the printed result and adjust as needed until the perfect settings are found.
+
+## To generate the gcode simply run:
+- python3 generate_linearadvance_gcode.py > outputfile.gcode
+
+# How to read/use the result:
+
+Here is the output for me for 0.0 to 1.0:
+![0 to 1.0](images/0-1.0.jpg)
+
+Based upon this I concluded that I needed somewhere between 0.3 and 0.6:
+![0.3 to 0.6](images/0.3-0.6.jpg)
+
+At/around 0.4 I started seeing issues so I generated another with just 0.3 to 0.4:
+![0.3 to 0.4](images/0.3-0.4.jpg)
+
+Based upon this result I needed this to be between 0.325 and 0.375 so I generated one more time to find out where:
+![0.325 to 0.375](images/0.325-0.375.jpg)
+
+I ended up settling with 0.350 as thats where the artifacts disappeared and started reappearing shortly there after.
---
+++ generate_linearadvance_gcode.py
@@ -0,0 +1,149 @@
+from math import *
+
+""" Tuneables """
+# extrusion parameters (mm)
+extrusion_width = 0.4
+layer_height = 0.2
+filament_diameter = 1.75
+hotend_temp = 205
+bed_temp = 60
+
+# print speeds (mm/s)
+travel_speed = 150
+first_layer_speed = 20
+slow_speed = 5
+fast_speed = 70
+
+# calibration object dimensions (mm)
+layers = 100
+object_width = 100
+num_patterns = 4
+pattern_width = 5
+
+# pressure advance gradient (s)
+pressure_advance_min = 0.000
+pressure_advance_max = 1.000
+
+# center of print bed (mm)
+offset_x = 150
+offset_y = 150
+
+# alerting
+alerting = 'no'
+#alerting = 'yes'
+
+# Load mesh, valid values are: Duet, Marlin or None
+mesh_type = None
+# mesh_type = 'Marlin'
+# mesh_type = 'Duet'
+
+""" end tuneables """
+
+def extrusion_volume_to_length(volume):
+    return volume / (filament_diameter * filament_diameter * 3.14159 * 0.25)
+
+def extrusion_for_length(length):
+    return extrusion_volume_to_length(length * extrusion_width * layer_height)
+layer0_z = layer_height
+curr_x = offset_x
+curr_y = offset_y
+curr_z = layer0_z
+
+# setting the temp
+print(f"M140 S{bed_temp} ; setting bed temp and continue")
+print(f"M104 S{hotend_temp} ; set nozzle temp and continue")
+print(f"M190 S{bed_temp} ; block for bed temp")
+print(f"M109 S{hotend_temp} ; block for nozzle temp")
+
+# home the printer
+print("G28 ; homing printer")
+
+if mesh_type != None:
+    if mesh_type == 'Duet':
+        print("G29 S1 ; loading mesh from Slot 1")
+    else:
+        print("M420 S1 ; loading mesh from Slot 1")
+
+
+# nozzle priming
+print("""G1 Z15.0 F6000 ; Move the platform down 15mm
+G92 E0 ; Reset Extruder
+G1 X10.1 Y20 Z0.28 F5000.0 ; Move to start position
+G1 X10.1 Y200.0 Z0.28 F1500.0 E15 ; Draw the first line
+G1 X10.4 Y200.0 Z0.28 F5000.0 ; Move to side a little
+G1 X10.4 Y20 Z0.28 F1500.0 E30 ; Draw the second line
+G92 E0 ; Reset Extruder
+G1 Z15 ; Lower Z to 15mm
+G1 X150 Y150 ; Goto X150 and Y150 for printing
+""")
+
+# goto z height
+print(f"G1 X{curr_x:.3f} Y{curr_y:.3f} Z{curr_z:.3f} F{(travel_speed * 60):.0f}")
+
+def up():
+    global curr_z
+    curr_z += layer_height
+    print(f"G1 Z{curr_z:.3f}")
+
+
+def line(x, y, speed):
+    length = sqrt(x ** 2 + y ** 2)
+    global curr_x, curr_y
+    curr_x += x
+    curr_y += y
+    if speed > 0:
+        print(f"G1 X{curr_x:3f} Y{curr_y:.3f} E{(extrusion_for_length(length)):.4f} F{(speed * 60):.0f}")
+    else:
+        print(f"G1 X{curr_x:3f} Y{curr_y:.3f} F{(travel_speed * 60):0f}")
+
+def goto(x, y):
+    global curr_x, curr_y
+    curr_x = x + offset_x
+    curr_y = y + offset_y
+    print(f"G1 X{curr_x:3f} Y{curr_y:3f}")
+
+line(-object_width / 2, 0, 0)
+
+for l in range(2):
+    for offset_i in range(5):
+        offset = offset_i * extrusion_width
+        line(object_width + offset, 0, first_layer_speed)
+        line(0, extrusion_width + offset * 2, first_layer_speed)
+        line(-object_width - offset * 2, 0, first_layer_speed)
+        line(0, -extrusion_width - offset * 2, first_layer_speed)
+        line(offset, 0, first_layer_speed)
+        line(0, -extrusion_width, 0)
+    up()
+    goto(-object_width / 2, 0)
+
+segment = (object_width * 1.0) / num_patterns
+space = segment - pattern_width
+
+for l in range(layers):
+    pressure_advance = (l / (layers * 1.0)) * (pressure_advance_max - pressure_advance_min) + pressure_advance_min
+    if l == 2:
+        print("M106 S1 ; turn on part cooling fan")
+    print(f"; layer {l:d}, pressure advance: {pressure_advance:.3f}")
+    if alerting == 'yes':
+        print(f"""M117 starting layer {l}")
+        M117 Pressure/Linear Advance set to: {pressure_advance}""")
+    print(f"M572 D0 S{pressure_advance:.3f}")
+    for i in range(num_patterns):
+        line(space / 2, 0, fast_speed)
+        line(pattern_width, 0, slow_speed)
+        line(space / 2, 0, fast_speed)
+    line(0, extrusion_width, fast_speed)
+    for i in range(num_patterns):
+        line(-space / 2, 0, fast_speed)
+        line(-pattern_width, 0, slow_speed)
+        line(-space / 2, 0, fast_speed)
+    line(0, -extrusion_width, fast_speed)
+    up()
+
+print("""M106 S0 ; turn off part cooling fan
+G91 Z ; set Z to relative position
+G1 Z30 ; lower Z to 30
+G28 X ; home X
+G28 Y ; home Y
+M104 S0 ; turn off hotend
+M190 S0 ; turn off bed""")
